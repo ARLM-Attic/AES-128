3eeb73493214d61bcfd51e32c00eb52294f1556c
Maksim Tsvetkov <max.tsvetkov@yandex.ru>
2016-11-19 20:43:17 +0300

    easy interface

--- AES/Form1.Designer.cs
+++ AES/Form1.Designer.cs
@@ -33,6 +33,16 @@
             this.btn_generate = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.tb_roundKey = new System.Windows.Forms.TextBox();
+            this.tb_plainText = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tb_ciphertext = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tb_binPlainText = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tb_binCipherText = new System.Windows.Forms.TextBox();
+            this.btn_encrypt = new System.Windows.Forms.Button();
+            this.btn_decrypt = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -75,21 +85,122 @@
             this.tb_roundKey.Location = new System.Drawing.Point(79, 38);
             this.tb_roundKey.Multiline = true;
             this.tb_roundKey.Name = "tb_roundKey";
-            this.tb_roundKey.Size = new System.Drawing.Size(490, 144);
+            this.tb_roundKey.Size = new System.Drawing.Size(783, 80);
             this.tb_roundKey.TabIndex = 4;
+            // 
+            // tb_plainText
+            // 
+            this.tb_plainText.Location = new System.Drawing.Point(79, 124);
+            this.tb_plainText.Multiline = true;
+            this.tb_plainText.Name = "tb_plainText";
+            this.tb_plainText.Size = new System.Drawing.Size(373, 144);
+            this.tb_plainText.TabIndex = 6;
+            this.tb_plainText.TextChanged += new System.EventHandler(this.tb_plainText_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Plain text:";
+            // 
+            // tb_ciphertext
+            // 
+            this.tb_ciphertext.Location = new System.Drawing.Point(79, 274);
+            this.tb_ciphertext.Multiline = true;
+            this.tb_ciphertext.Name = "tb_ciphertext";
+            this.tb_ciphertext.Size = new System.Drawing.Size(373, 144);
+            this.tb_ciphertext.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 277);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Сiphertext:";
+            // 
+            // tb_binPlainText
+            // 
+            this.tb_binPlainText.Location = new System.Drawing.Point(489, 124);
+            this.tb_binPlainText.Multiline = true;
+            this.tb_binPlainText.Name = "tb_binPlainText";
+            this.tb_binPlainText.Size = new System.Drawing.Size(373, 144);
+            this.tb_binPlainText.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(458, 127);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(25, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Bin:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(458, 277);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(25, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Bin:";
+            // 
+            // tb_binCipherText
+            // 
+            this.tb_binCipherText.Location = new System.Drawing.Point(489, 274);
+            this.tb_binCipherText.Multiline = true;
+            this.tb_binCipherText.Name = "tb_binCipherText";
+            this.tb_binCipherText.Size = new System.Drawing.Size(373, 144);
+            this.tb_binCipherText.TabIndex = 11;
+            this.tb_binCipherText.TextChanged += new System.EventHandler(this.tb_binCipherText_TextChanged);
+            // 
+            // btn_encrypt
+            // 
+            this.btn_encrypt.Location = new System.Drawing.Point(79, 429);
+            this.btn_encrypt.Name = "btn_encrypt";
+            this.btn_encrypt.Size = new System.Drawing.Size(373, 40);
+            this.btn_encrypt.TabIndex = 13;
+            this.btn_encrypt.Text = "Encrypt";
+            this.btn_encrypt.UseVisualStyleBackColor = true;
+            this.btn_encrypt.Click += new System.EventHandler(this.btn_encrypt_Click);
+            // 
+            // btn_decrypt
+            // 
+            this.btn_decrypt.Location = new System.Drawing.Point(489, 429);
+            this.btn_decrypt.Name = "btn_decrypt";
+            this.btn_decrypt.Size = new System.Drawing.Size(373, 40);
+            this.btn_decrypt.TabIndex = 14;
+            this.btn_decrypt.Text = "Decrypt";
+            this.btn_decrypt.UseVisualStyleBackColor = true;
+            this.btn_decrypt.Click += new System.EventHandler(this.btn_decrypt_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(581, 383);
+            this.ClientSize = new System.Drawing.Size(885, 497);
+            this.Controls.Add(this.btn_decrypt);
+            this.Controls.Add(this.btn_encrypt);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tb_binCipherText);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tb_binPlainText);
+            this.Controls.Add(this.tb_ciphertext);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tb_plainText);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.tb_roundKey);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.btn_generate);
             this.Controls.Add(this.tb_key);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "AES-128";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -102,6 +213,16 @@
         private System.Windows.Forms.Button btn_generate;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox tb_roundKey;
+        private System.Windows.Forms.TextBox tb_plainText;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tb_ciphertext;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tb_binPlainText;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tb_binCipherText;
+        private System.Windows.Forms.Button btn_encrypt;
+        private System.Windows.Forms.Button btn_decrypt;
     }
 }
 
--- AES/Form1.cs
+++ AES/Form1.cs
@@ -12,16 +12,11 @@ namespace AES
 {
     public partial class Form1 : Form
     {
+        Aes aes;
+
         public Form1()
         {
             InitializeComponent();
-
-            byte[] test = { 0x19, 0xa0, 0x9a, 0xe9, 0x3d, 0xf4, 0xc6, 0xf8, 0xe3, 0xe2, 0x8d, 0x48, 0xbe, 0x2b, 0x2a, 0x08 };
-            for (var i = 0; i < test.Length; i++)
-            {
-                test[i] = Aes.SubBytes(test[i]);
-            }
-
         }
 
         private byte[] StringToByte(string str)
@@ -34,11 +29,64 @@ namespace AES
             byte[] generation128 = new byte[16];
             Random rnd = new Random();
             rnd.NextBytes(generation128);
-
-            Aes test = new Aes(generation128);
-            tb_roundKey.Text = BitConverter.ToString(test.KeyExpansion());
-
             tb_key.Text = BitConverter.ToString(generation128);
+
+            //Aes test = new Aes(new byte[] { 0x2b, 0x28, 0xab, 0x09, 0x7e, 0xae, 0xf7, 0xcf, 0x15, 0xd2, 0x15, 0x4f, 0x16, 0xa6, 0x88, 0x3c });
+            //var result = test.Encrypt(new byte[] { 0x32, 0x88, 0x31, 0xe0, 0x43, 0x5a, 0x31, 0x37, 0xf6, 0x30, 0x98, 0x07, 0xa8, 0x8d, 0xa2, 0x34 });
+            //var result2 = test.Decrypt(result);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_encrypt_Click(object sender, EventArgs e)
+        {
+            var aes = new Aes(StringToByte(tb_key.Text));
+            tb_roundKey.Text = BitConverter.ToString(aes.KeyExpansion());
+
+            tb_binCipherText.Text = BitConverter.ToString(aes.Encrypt(StringToByte(tb_binPlainText.Text)));
+
+        }
+
+        private void btn_decrypt_Click(object sender, EventArgs e)
+        {
+            var aes = new Aes(StringToByte(tb_key.Text));
+            tb_roundKey.Text = BitConverter.ToString(aes.KeyExpansion());
+
+            var input = StringToByte(tb_binCipherText.Text);
+
+            if (input.Length % 16 != 0)
+            {
+                var temp = new byte[input.Length + (16 - input.Length % 16)];
+                Array.Copy(input, temp, input.Length);
+                input = temp;
+            }
+
+            tb_plainText.Text = Encoding.UTF8.GetString(aes.Decrypt(input));
+        }
+
+        private void tb_plainText_TextChanged(object sender, EventArgs e)
+        {
+            tb_binPlainText.Text = BitConverter.ToString(Encoding.UTF8.GetBytes(tb_plainText.Text));
+        }
+
+        private void tb_binCipherText_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                tb_ciphertext.Text = Convert.ToBase64String(StringToByte(tb_binCipherText.Text));
+            }
+            catch
+            {
+                if(tb_binCipherText.Text.Length == 0)
+                {
+                    tb_ciphertext.Text = "";
+                }
+                return;
+            }
+            
         }
     }
 }
